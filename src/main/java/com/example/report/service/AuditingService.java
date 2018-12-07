ab05aeabb25b3fa3782e6487ed83930af954d03a
wyz <wyz0175@163.com>
2018-12-07 16:45:13 +0800

    2018/12/07 审批

--- src/main/java/com/example/report/service/AuditingService.java
+++ src/main/java/com/example/report/service/AuditingService.java
@@ -1,5 +1,5 @@
 package com.example.report.service;
 
 public interface AuditingService {
-
+    //List<Project> showAllProject(int uid);
 }
--- src/main/java/com/example/report/service/impl/AuditingServiceImp.java
+++ src/main/java/com/example/report/service/impl/AuditingServiceImp.java
@@ -1,4 +1,9 @@
 package com.example.report.service.impl;
 
-public class AuditingServiceImp {
+import com.example.report.service.AuditingService;
+
+import javax.annotation.Resource;
+
+public class AuditingServiceImp implements AuditingService {
+    //@Resource
 }
