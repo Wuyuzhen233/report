7b3601495f0eabaf2ca8b04cd9c36fbe9a2414b9
aimu_zs <1422203188@qq.com>
2018-12-07 16:46:37 +0800

    admin function181207

--- src/main/java/com/example/report/common/enums/ErrorCode.java
+++ src/main/java/com/example/report/common/enums/ErrorCode.java
@@ -3,45 +3,49 @@ package com.example.report.common.enums;
 
 public enum ErrorCode {
 
-  /**
-   * (保留码) 表示未知异常
-   */
-  UNEXCEPTED(9999),
+    /**
+     * (保留码) 表示未知异常
+     */
+    UNEXCEPTED(9999),
 
+    /**
+     * 成功
+     */
+    SUCCESS(1000),
 
-  /**
-   * 成功
-   */
-  SUCCESS(1000),
+    /**
+     * 用户未登录
+     */
+    USER_NOT_LOGIN(1001),
 
-  /**
-   * 用户未登录
-   */
-  USER_NOT_LOGIN(1001),
+    /**
+     * 用户不存在
+     */
+    USER_NOT_EXISTS(1002),
 
+    /**
+     * 入库失败
+     */
+    FAIL_DATABASE(1003),
 
-  /**
-   * 用户不存在
-   */
-  USER_NOT_EXISTS(2001),
+    /**
+     * 登录信息不完整
+     */
+    LOGIN_INFO_INCOMPLETE(1004),
 
+    /**
+     * 入参不完整
+     */
+    PARAMS_INCOMPLETE(1005);
 
+    private int value;
 
-  /**
-   * 登录信息不完整
-   */
-  LOGIN_INFO_INCOMPLETE(5011);
+    private ErrorCode(int value) {
+        this.value = value;
+    }
 
-
-
-  private int value;
-
-  private ErrorCode(int value) {
-    this.value = value;
-  }
-
-  public int getValue() {
-    return value;
-  }
+    public int getValue() {
+        return value;
+    }
 }
 
