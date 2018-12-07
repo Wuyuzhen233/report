f068fe93e046905e0aed27d9ac5226a5bffff623
aimu_zs <1422203188@qq.com>
2018-12-07 16:34:21 +0800

    admin function181207

--- src/main/java/com/example/report/mapper/UserMapper.java
+++ src/main/java/com/example/report/mapper/UserMapper.java
@@ -6,7 +6,12 @@ import tk.mybatis.mapper.common.Mapper;
 
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper extends Mapper<User> {
     List<User> queryUserByNameAndPassWord(@Param("phone") String phone, @Param("password") String password);
+
+    String findUserIdByPhonePwd(Map<String,String> map);
+
+    void resetPwd(Map<String,String> map);
 }
